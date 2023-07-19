0d3776d2be39136d47c3c8d15be79e8e3e04cb23
Your Name <keriwest88@gmail.com>
2023-07-18 23:00:41 -0400

    lesson 6.1 added projects section, nav menu and image drop shadow to index.js. Added more styles to index.css and added projects and nav section to index.html.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,59 @@
 /* script to make sure HTML loads before script 
   (script can be placed anywhre on the page)  */
 document.addEventListener("DOMContentLoaded", () => {
+  
+  /*
+     =======================================  
+     ***********    NAV BAR    *************
+     =======================================
+   */
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-  /*  ///////////// SKILLS SECTION  /////////////  */
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-item").forEach((n) =>
+    n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+
+  /*
+     ========================================== 
+     ************ ABOUT SECTION  *************
+     ==========================================
+   */
+    const dropShadow = document.getElementById("img");
+    dropShadow.style.setProperty("-webkit-filter", "drop-shadow(12px 12px 10px #072b3d)");
+    
+   
+
+  /*
+     ========================================== 
+     ************ SKILLS SECTION  *************
+     ==========================================
+   */
   const skills = [
     "HTML",
     "CSS",
-    "JavaScript",
+    "JavaScript [node.js]",
     "Visual Studio Code",
     "GitHub",
     "Adobe Creative Cloud",
     "eCommerce Management",
-    "CMS (BigCommerce/ Webflow/ Wordpress)",
-    "SEO (semrush)",
-    "Google Analytics", 
+    "SEO [semrush]",
+    "Google Analytics",
     "Google Search Console",
     "NetSuite",
-    "Microsoft Office",
-    "Project Management (Jira/ Asana/ Wrike)",
-    "Email Marketing (Klaviyo/Mailchimp)",
+    "Microsoft Office Suite",
+    "Content Management Systems",
+    "BigCommerce | Webflow | Wordpress",
+    "PM [Jira | Asana | Wrike | Trello]",
+    "Email Marketing [Klaviyo | Mailchimp]",
   ];
   const skillsSection = document.getElementById("skills");
   skillsList = skillsSection.querySelector("ul");
@@ -27,10 +62,40 @@ document.addEventListener("DOMContentLoaded", () => {
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
   }
-  
 
-  
-  /*   ///////////// MESSAGE FORM SECTION /////////////   */
+  /*
+      ====================================  
+      ************ PROJECTS  *************
+      ====================================
+   */
+  const githubRequest = new XMLHttpRequest();
+
+  githubRequest.addEventListener("load", function () {
+    if (githubRequest.readyState === 4 && githubRequest.status === 200) {
+      let repositories = JSON.parse(this.response);
+      const projectSection = document.getElementById("projects");
+      const projectList = projectSection.querySelector("ul");
+
+      for (let i = 0; i < repositories.length; i++) {
+        const project = document.createElement("li");
+        // change list to buttons that link to github
+        const repoLinks = document.createElement("button");
+        repoLinks.href = repositories[i].html_url;
+        repoLinks.textContent = repositories[i].name;
+
+        project.appendChild(repoLinks);
+        projectList.appendChild(project);
+      }
+    }
+  });
+  githubRequest.open("GET", "https://api.github.com/users/wessstt/repos");
+  githubRequest.send();
+
+  /*
+     ================================================
+     ************ MESSAGE FORM SECTION **************
+     ================================================
+   */
   /*  Hide message header on load  */
   document.getElementById("messages").style.display = "none";
   /* FORM */
@@ -69,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const entry = e.target.parentNode;
       messageList.removeChild(entry);
       if (messageList.length === true) {
-        messages.style.display = "";
+        messages.style.display = "block";
       } else {
         messages.style.display = "none";
       }
@@ -80,7 +145,11 @@ document.addEventListener("DOMContentLoaded", () => {
     messageForm.reset();
   });
 
-  /*  ///////////// FOOTER SECTION /////////////   */
+  /*
+     =========================================  
+     ************ FOOTER SECTION *************
+     =========================================  
+   */
   const today = new Date();
   const thisYear = today.getFullYear();
   const footer = document.querySelector("footer");
@@ -88,39 +157,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   copyright.innerHTML = `🏳️‍⚧️ safe space 🏳️‍🌈 | &copy; Keri West ${thisYear}`;
   footer.appendChild(copyright);
- 
+
   /*  /// Styling ///   */
   copyright.style.fontSize = "11px";
   copyright.style.wordSpacing = "1px";
   copyright.style.textAlign = "center";
   copyright.style.color = "#298c6f";
   copyright.style.fontWeight = "300";
-
-
-/*  //////////// NAV BAR  ///////////// */
-  const menu = document.querySelector(".menu");
-  const menuItems = document.querySelectorAll(".menuItem");
-  const hamburger = document.querySelector(".hamburger");
-  const closeIcon = document.querySelector("closeIcon");
-  const menuIcon = document.querySelector(".menuIcon")
-
-  function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-      menu.classList.remove("showMenu");
-      closeIcon.style.display = "block";
-      hamburger.style.display = "none";
-    } else {
-      menu.classList.add("showMenu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
-    }
-  } hamburger.addEventListener("click", toggleMenu);
-  
-  menuItems.forEach(
-    function(menuItem) {
-      menuItem.addEventListener("click", toggleMenu);
-    }
-  )
-
+  copyright.style.marginBottom = "1rem";
 });
-
